857631cfe422cdd0abd85a9977a1b8b9c497d11b
shuai.qi <shuai.qi@okcoin.com>
2019-03-16 15:31:26 +0800

    Exclude Nulldata and Nonstandard

--- max-script-lenght/main.go
+++ max-script-lenght/main.go
@@ -1,8 +1,10 @@
-package max_script_lenght
+package main
 
 import (
 	"fmt"
+	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/rpcclient"
+	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
 	"log"
 	"time"
@@ -12,9 +14,9 @@ var max int
 
 func main() {
 	connCfg := &rpcclient.ConnConfig{
-		Host:         "127.0.0.1:8332",
-		User:         "hello",
-		Pass:         "world",
+		Host:         "127.0.0.1:9332",
+		User:         "okcoin",
+		Pass:         "lZWMxOThhODEyNDQyYTg0NjY",
 		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
 		DisableTLS:   true, // Bitcoin core does not provide TLS by default
 	}
@@ -24,13 +26,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer client.Shutdown()
 
 	blockChan := make(chan *wire.MsgBlock, 200)
 	go func() {
 		for i := 0; i < 567297; i++ {
 			if i%1000 == 0 {
-				fmt.Printf("%s Handle block height: %d, channel size: %d", time.Now().String(), i, len(blockChan))
+				fmt.Printf("%s Handle block height: %d, channel size: %d, max length: %d\n", time.Now().String(), i, len(blockChan), max)
 			}
 
 			blockhash, err := client.GetBlockHash(int64(i))
@@ -51,18 +52,31 @@ func main() {
 
 	for block := range blockChan {
 		for _, tx := range block.Transactions {
-			for _, out := range tx.TxOut {
+			for idx, out := range tx.TxOut {
 				// null data
 				if out.PkScript[0] == 0x6a {
 					continue
 				}
 
+				class, _, _, _ := txscript.ExtractPkScriptAddrs(out.PkScript, &chaincfg.MainNetParams)
+				if class != txscript.PubKeyHashTy &&
+					class != txscript.WitnessV0PubKeyHashTy &&
+					class != txscript.PubKeyTy &&
+					class != txscript.ScriptHashTy &&
+					class != txscript.WitnessV0ScriptHashTy &&
+					class != txscript.MultiSigTy {
+
+					continue
+				}
+
 				if len(out.PkScript) > max {
+					fmt.Printf("transaction: %s:%d\n", tx.TxHash(), idx)
 					max = len(out.PkScript)
 				}
 			}
 		}
 	}
 
+	client.Shutdown()
 	fmt.Println("most length in pkScript(byte):", max)
 }
